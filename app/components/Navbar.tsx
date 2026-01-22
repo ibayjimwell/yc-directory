54bb6b14cea0093a2f8356858a19566177a04e81
ibayjimwell <ibayjimwell@gmail.com>
2026-01-22 03:02:56 +0000

    Implement auth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import { auth, signOut, signIn } from '@/auth'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <>
       <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
@@ -17,8 +20,38 @@ const Navbar = () => {
             />
           </Link>
 
-          <div className='flex items-center gap-5'>
+          <div className='flex items-center gap-5 text-black'>
+            {session && session?.user ? (
+              <>
+                
+                <Link href='/startup/create'>
+                  <span >Create</span>
+                </Link>
 
+                <form action={async () => {
+                  'use server';
+                  await signOut({redirectTo: '/'})
+                }}>
+                  <button type='submit'>
+                    Logout
+                  </button>
+                </form>
+
+                <Link href={`/user/${session?.user?.id}`}>
+                  <span>{ session?.user?.name }</span>
+                </Link>
+
+              </>
+            ) : (
+                <form action={async () => {
+                    'use server';
+                    await signIn('github')
+                }}>
+                  <button type='submit'>
+                    Login
+                  </button>
+                </form>
+              ) }
           </div>
 
         </nav>
