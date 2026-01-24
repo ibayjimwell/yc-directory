ce4e77b954bc8529dc7bdc67e17e9b19114d6ef6
ibayjimwell <ibayjimwell@gmail.com>
2026-01-24 01:42:16 +0000

    Create the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <>
-      <div className="text-2xl">Home</div>
+      <section className='pink_container'>
+        <h1 className='heading'>Pitch your startup, connect with entrepreneurs</h1>
+        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions</p>
+      </section>
     </>
   )
 }
